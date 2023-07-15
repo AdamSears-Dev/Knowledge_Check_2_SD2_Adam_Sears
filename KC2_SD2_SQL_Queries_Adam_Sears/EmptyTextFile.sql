945e1a5fa2d92a7f424ab2ba27b4317bc8706aa3
Adam Sears <adam_sears031@hotmail.com>
2023-07-15 16:19:20 -0400

    SQL Queries
    Added SQL file with two queries that retrieve customer name and supplier address.

---
+++ KC2_SD2_SQL_Queries_Adam_Sears/EmptyTextFile.sql
@@ -0,0 +1,14 @@
+﻿SELECT Customers.CustomerName
+FROM Customers
+INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+WHERE Orders.OrderID = 10310;
+
+-- Result: CustomerName: The Big Cheese
+
+SELECT Suppliers.Address
+FROM Suppliers
+INNER JOIN Products ON Suppliers.SupplierID = Products.SupplierID
+WHERE Products.ProductID = 40;
+
+-- Result: Address: Order Processing Dept. 2100 Paul Revere Blvd.
+
